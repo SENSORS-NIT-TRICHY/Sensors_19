78c29495ed15e5a4f5dfcfb5e6d49a1868bd67d4
kaavin <kaavinak@gmail.com>
2021-12-24 19:36:12 +0530

    winners page works

--- app/functions/stickynavbar.js
+++ app/functions/stickynavbar.js
@@ -1,20 +1,24 @@
 // When the user scrolls the page, execute myFunction
 
-if(window.location.pathname.includes('index.html') || window.location.pathname.includes('covid_tech.html'))
+if(window.location.pathname.includes('index.html') || window.location.pathname.includes('covid_tech.html') || window.location.pathname.includes('winners.html'))
 {
-  
+ 
   window.onscroll = function() {
-    console.log("scrolling normally")
+   
     myFunction('main')};
 
 }
 else
 {
-    //console.log("bruh");
+    
     let uicont = document.querySelector("body")
-    uicont.addEventListener("scroll",function() {
-        console.log("scrolling not  normally")
-        myFunction('notmain')})
+   
+    
+      uicont.addEventListener("scroll",function() {
+         
+          myFunction('notmain')})
+    
+   
 }
 // Get the navbar
 var navbar = document.querySelector(".navbar.navbar-expand-lg");
@@ -24,14 +28,14 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction(status) {
-//    console.log("come on mane")
-    console.log(navbar.getBoundingClientRect().top)
+
+   
   if(status=='main')
   {
     if (window.pageYOffset !=0) {
         if(!navbar.classList.add("sticky"))
         {
-            console.log("sticky")
+            
              
           
           navbar.classList.add("sticky")
@@ -41,7 +45,7 @@ function myFunction(status) {
   
         }
     } else {
-        console.log("top")
+        
       navbar.classList.remove("sticky");
       navbar.style.position=null
           navbar.style.boxSizing=null
@@ -52,7 +56,7 @@ function myFunction(status) {
   {
       try{
         let bodydiv = document.querySelector('body')
-        console.log('bruh',bodydiv.getBoundingClientRect().top)
+      
         if(bodydiv.getBoundingClientRect().top==0 )
         {
      navbar.classList.remove("sticky");
@@ -63,7 +67,7 @@ function myFunction(status) {
       if (navbar.getBoundingClientRect().top !=0) {
           if(!navbar.classList.add("sticky"))
           {
-         //     console.log("sticky")
+        
                
            
             navbar.classList.add("sticky")
@@ -74,23 +78,15 @@ function myFunction(status) {
     
           }
       } else {
-      ///    console.log("top")
+     
       
       }
       }
       catch(e)
       {
-       console.log('error ',e)
+       
       }
       
   }
   
 }
-// .sticky {
-//     position: fixed;
-//     box-sizing: border-box;
-//     top: 0;
-//     border: #007bb7 1px solid;
-//     width: 100%;
-//    z-index: 2;
-   
