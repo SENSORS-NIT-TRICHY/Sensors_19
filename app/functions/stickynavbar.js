64ee3091c6a387608e2b26d7a136ac4c222d9c6e
kaavin <kaavinak@gmail.com>
2021-12-24 14:54:12 +0530

    made the navbar opaque

--- app/functions/stickynavbar.js
+++ app/functions/stickynavbar.js
@@ -1,15 +1,18 @@
 // When the user scrolls the page, execute myFunction
 
-if(window.location.pathname.includes('index.html') || window.location.pathname.includes('covid_tech.html'))
+if(window.location.pathname.includes('index.html') || window.location.pathname.includes('covid_tech.html') || window.location.pathname.includes('winners.html'))
 {
-    window.onscroll = function() {myFunction('main')};
+    window.onscroll = function() {//console.log("scrolling normally")
+    myFunction('main')};
 
 }
 else
 {
-    console.log("bruh");
+    //console.log("bruh");
     let uicont = document.querySelector("body")
-    uicont.addEventListener("scroll",function() {myFunction('notmain')})
+    uicont.addEventListener("scroll",function() {
+  //      console.log("scrolling normally")
+        myFunction('notmain')})
 }
 // Get the navbar
 var navbar = document.querySelector(".navbar.navbar-expand-lg");
@@ -19,7 +22,7 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction(status) {
-    console.log("come on mane")
+//    console.log("come on mane")
     console.log(navbar.getBoundingClientRect().top)
   if(status=='main')
   {
@@ -44,30 +47,37 @@ function myFunction(status) {
   }
   if(status=='notmain')
   {
-      let bodydiv = document.querySelector('.ui.cont')
-      console.log(bodydiv.getBoundingClientRect().top)
-      if(bodydiv.getBoundingClientRect().top==121)
-      {
-   navbar.classList.remove("sticky");
-      navbar.style.position=null
-          navbar.style.boxSizing=null
-   
-      }
-    if (navbar.getBoundingClientRect().top !=0) {
-        if(!navbar.classList.add("sticky"))
+      try{
+        let bodydiv = document.querySelector('.ui.cont')
+        console.log(bodydiv.getBoundingClientRect().top)
+        if(bodydiv.getBoundingClientRect().top>=90 )
         {
-            console.log("sticky")
-             
-          
-          navbar.classList.add("sticky")
-          navbar.style.position='fixed'
-          navbar.style.boxSizing='border-box'
-  
+     navbar.classList.remove("sticky");
+        navbar.style.position=null
+            navbar.style.boxSizing=null
+     
         }
-    } else {
-        console.log("top")
+      if (navbar.getBoundingClientRect().top !=0) {
+          if(!navbar.classList.add("sticky"))
+          {
+         //     console.log("sticky")
+               
+            
+            navbar.classList.add("sticky")
+            navbar.style.position='fixed'
+            navbar.style.boxSizing='border-box'
     
-    }
+          }
+      } else {
+      ///    console.log("top")
+      
+      }
+      }
+      catch(e)
+      {
+    //   console.log('error ',e)
+      }
+      
   }
   
 }
